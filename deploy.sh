ae7532197dea974805388435a5b0efb99251e279
sutianyu <sutianyu@xiaohongshu.com>
2025-02-06 16:13:42 +0800

    up

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
+egp_url='https://gitee.com/styUnv/tv/raw/gd/e.xml'
+icon_prefix='https://gitee.com/styUnv/tv/raw/gd/icon/'
+save_threshold=50
+
 
 TMP_DIR='/tmp/tv_tmp'
-
 fanming_git=$TMP_DIR/fanming_live
 iplist_git=$TMP_DIR/iptv_api
-egp_url='https://gitee.com/styUnv/tv/raw/master/e.xml'
-icon_prefix='cc/icon/'
-
 mkdir $TMP_DIR
 
 
@@ -28,7 +28,22 @@ input_file=$iplist_git/output/result.m3u
 output_file="live.m3u"
 sed -i.bak "s#x-tvg-url=\"[^\"]*\"#x-tvg-url=\"$egp_url\"#" "$input_file"
 sed -i.bak "s#https://raw.githubusercontent.com/fanmingming/live/main/tv/#$escaped_icon_prefix#g" "$input_file"
-
-
 cp "$input_file" "$output_file"
+
+
+count=$(grep -o "tvg-name" "$output_file" | wc -l)
+count=$(echo $count | tr -d ' ')
+
+if [ $count -gt $save_threshold ]; then
+    echo "最新频道个数为 $count，大于阈值 $save_threshold"
+    if ! git diff --staged --quiet; then
+        echo "开始执行 git push 操作"
+        git add -A
+        git commit -a -m "update live source"
+        git push --force
+    fi
+else
+    echo "最新频道个数为 $count，未超过阈值 $save_threshold，不执行 git push 操作。"
+fi
+
 rm $TMP_DIR
